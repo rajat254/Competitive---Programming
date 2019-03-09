d621ecc258e7d12b54de1f8008ccfb6539c6c175
Rajat Pal <pal.rajat1997@gmail.com>
2019-03-10 03:48:14 +0530

    Update CSUMQ.py

--- SPOJ/CSUMQ.py
+++ SPOJ/CSUMQ.py
@@ -1,6 +1,11 @@
 from itertools import accumulate
 n=int(input())
 a=list(map(int,input().rstrip().split()))
+b=list(accumulate(a))
+q=int(input())
+for i in range(q):
+    l,r=list(map(int,input().split()))
+    if l==r:
         print(a[l])
     else:
         print(b[r]-b[l]+a[l])
