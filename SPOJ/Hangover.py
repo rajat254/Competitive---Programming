588569643682e8adde79684427122a53f36a27f8
Rajat Pal <pal.rajat1997@gmail.com>
2019-04-13 03:30:58 +0530

    Added Hangover.py
    Added Hangover spoj problem in python 3

---
+++ SPOJ/Hangover.py
@@ -0,0 +1,17 @@
+import math
+while(1):
+    n=float(input())
+    if n==0.0:
+        exit()
+    else:
+        g=1
+        sum1=0
+        if 1==1:
+            i=1
+            while(1):
+                g+=1
+                sum1+=1/g
+                if sum1>=n:
+                    break
+            print(g-1,'card(s)')
+        
